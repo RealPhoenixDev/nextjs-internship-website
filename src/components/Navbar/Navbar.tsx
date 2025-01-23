76ae2500213795053685ce472791dd26d0de99f6
PhoenixDev <realphoenixdev@gmail.com>
2025-01-23 12:51:48 +0200

    navbar work

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,24 +11,38 @@ import {
   NavbarMenuItem,
   Button,
   Link,
+  Image,
 } from "@heroui/react";
 
 export default function Navbar() {
   return (
-    <Nav className="dark bg-[#0f0f0f] border-b-1 border-[#6e6e6e]">
-      <NavbarBrand>Some Logo!</NavbarBrand>
+    <Nav className="dark bg-opacity-40 z-10" height={64}>
+      <NavbarBrand>
+        <Image src="Company Logo White.svg" alt="Company logo" className="" />
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
       <NavbarContent>
-        <NavbarItem>Thing 1</NavbarItem>
-        <NavbarItem>Thing 2</NavbarItem>
-        <NavbarItem>Thing 3</NavbarItem>
+        <NavbarItem>
+          <Link href="/">Home</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/about">About Us</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/contacts">Contacts</Link>
+        </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
+        <NavbarItem>
+          <Button variant="bordered" as={Link} href="/auth/login">
+            Sign Up
+          </Button>
+        </NavbarItem>
         <NavbarItem>
           <Button as={Link} href="/auth/login">
             Log In
           </Button>
         </NavbarItem>
-        <NavbarItem>Sign up</NavbarItem>
       </NavbarContent>
     </Nav>
   );
